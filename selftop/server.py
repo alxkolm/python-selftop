f0b0f8584c9eee632ae490b127b3f7123694e373
alxkolm <alxkolm@gmail.com>
2017-04-05 23:16:58 +0300

    improve clusterization

--- selftop/server.py
+++ selftop/server.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+import os
 from datetime import date
 import tempfile
 import subprocess
@@ -21,10 +22,20 @@ def index():
 
     records = get_records()
 
-    titles = set([record['window_title'] for record in records if record['window_title'] != ''])
+    titles = {record['window_id']: record['window_title'] for record in records
+              if record['window_title'] != ''}
     cluster_map = get_title_clusters(titles)
 
-    useful_windows = [x for x in records if x['duration'] >= TRANSITION_MATRIX_DURATION_THRESHOLD]
+    mcl_cluster_map = mcl_clusters(records)
+
+    return {'processes': processes, 'windows': windows, 'records': records,
+            'titleClusters': cluster_map, 'mclClusters': mcl_cluster_map}
+
+
+def mcl_clusters(records):
+    """Returns window_id => claster_label dict"""
+    useful_windows = [x for x in records if
+                      x['duration'] >= TRANSITION_MATRIX_DURATION_THRESHOLD]
     matrix = {}
     prev = useful_windows[0]
     for curr in useful_windows[1:]:
@@ -33,13 +44,11 @@ def index():
             matrix[idx] = 0
         matrix[idx] += 1
         prev = curr
-
     _, file_data_path = tempfile.mkstemp(None, 'selftop_data')
     _, file_mci_path = tempfile.mkstemp()
     _, file_tab_path = tempfile.mkstemp()
     _, file_cluster_native_path = tempfile.mkstemp()
     _, file_cluster_path = tempfile.mkstemp()
-
     file_data = open(file_data_path, 'w')
     for idx, value2 in matrix.items():
         file_data.write('{}\t{}\t{}\n'.format(idx[0], idx[1], matrix[idx]))
@@ -47,43 +56,55 @@ def index():
     cmd = "mcxload " \
           " --stream-mirror" \
           " -abc {file_data_path}" \
-          " -o {filename_mci_path}" \
-          " -write-tab {filename_tab_path}" \
-        " && mcl" \
-          " {filename_mci_path}" \
-          " -o {filename_cluster_native}" \
-        " && mcxdump" \
-          " -icl {filename_cluster_native_path}" \
-          " -tabr {filename_tab_path}" \
-          " -o {filename_cluster_path}"
-    result = subprocess.run([
-        'mcxload',
-        '--stream-mirror',
-        " -abc {file_data_path}",
-        " -o {filename_mci_path}",
-        " -write-tab {filename_tab_path}"])
-    result = subprocess.run([
-        'mcl',
-        '{filename_mci_path}',
-        '-o {filename_cluster_native}'
-    ])
-    result = subprocess.run([
-        'mcxdump',
-        "-icl {filename_cluster_native_path}",
-        "-tabr {filename_tab_path}",
-        "-o {filename_cluster_path}"
-    ])
-
-
-
-    return {'processes': processes, 'windows': windows, 'records': records, 'titleClusters': cluster_map}
+          " -o {file_mci_path}" \
+          " -write-tab {file_tab_path}" \
+          " && mcl" \
+          " {file_mci_path}" \
+          " -o {file_cluster_native_path}" \
+          " && mcxdump" \
+          " -icl {file_cluster_native_path}" \
+          " -tabr {file_tab_path}" \
+          " -o {file_cluster_path}".format(
+            file_data_path=file_data_path,
+            file_mci_path=file_mci_path,
+            file_tab_path=file_tab_path,
+            file_cluster_native_path=file_cluster_native_path,
+            file_cluster_path=file_cluster_path
+            )
+    os.system(cmd)
+    class_label = 0
+    mcl_cluster_map = {}
+    with open(file_cluster_path, 'r') as file:
+        for line in file:
+            for window_id in line.split('\t'):
+                mcl_cluster_map[int(window_id)] = str(class_label)
+            class_label += 1
+    os.remove(file_data_path)
+    os.remove(file_mci_path)
+    os.remove(file_tab_path)
+    os.remove(file_cluster_native_path)
+    os.remove(file_cluster_path)
+    return mcl_cluster_map
 
 
 def get_title_clusters(titles):
-    titles_cluster_labels = string_clusterization.run(list(titles))
+    """Returns window_id => claster_label dict"""
+    keys = list(titles.keys())
+    values = list(titles.values())
+    titles_set = list(set(titles.values()))
+    titles_cluster_labels = string_clusterization.run(titles_set)
     cluster_map = {str(x[0]): int(x[1]) for x in
-                   zip(titles, titles_cluster_labels)}
-    return cluster_map
+                   zip(titles_set, titles_cluster_labels)}
+
+    def get_id_by_title(title_str):
+        return [keys[idx] for idx, v in enumerate(values) if v == title_str]
+
+    out = {}
+    for title, label in zip(titles_set, titles_cluster_labels):
+        for i in get_id_by_title(title):
+            out[i] = str(label)
+
+    return out
 
 
 def get_records():
@@ -101,10 +122,11 @@ def get_records():
       LEFT JOIN window ON window.id = record.window_id
       LEFT JOIN process ON process.id = window.process_id
     WHERE duration > 0
-    AND start >= ?
+    --AND start >= ?
     ORDER BY start;
     """
-    records = [dict(x) for x in cur.execute(sql, [date.today()]).fetchall()]
+    # records = [dict(x) for x in cur.execute(sql, [date.today()]).fetchall()]
+    records = [dict(x) for x in cur.execute(sql).fetchall()]
     return records
 
 
