1026c18f78d5a6d0e6d060ac085423bae796b90f
alexey.kolmakov <alxkolm@bk.ru>
2017-07-30 20:27:19 +0300

    Add hour of day to feature list

--- selftop/machine_learning/record_clusterization.py
+++ selftop/machine_learning/record_clusterization.py
@@ -1,6 +1,6 @@
 import numpy as np
 from kmodes import kmodes
-
+import dateutil.parser
 
 def run(records, mcl_clusters, title_clusters):
     output = []
@@ -8,11 +8,12 @@ def run(records, mcl_clusters, title_clusters):
         output.append((
             'title_' + str(title_clusters[record['window_id']] if record['window_id'] in title_clusters else -1),
             'mcl_' + str(mcl_clusters[record['window_id']] if record['window_id'] in mcl_clusters else -1),
-            'pid' + str(record['process_id'])
+            'pid' + str(record['process_id']),
+            dateutil.parser.parse(record['start']).hour
         ))
     # km4 = kmodes.KModes(n_clusters=4, init='Huang', n_init=5, verbose=1)
     # km5 = kmodes.KModes(n_clusters=5, init='Huang', n_init=5, verbose=1)
-    km6 = kmodes.KModes(n_clusters=6, init='Huang', n_init=5, verbose=1)
+    km6 = kmodes.KModes(n_clusters=10, init='Huang', n_init=5, verbose=1)
     X = np.array(output)
     T = X
     # clusters4 = km4.fit_predict(T)
